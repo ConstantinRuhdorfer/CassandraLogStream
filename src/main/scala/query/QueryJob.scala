0468f4d1bd1d6f2000d654cdc3d44cb10ba7ddbc
Constantin Ruhdorfer <constantin.ruhdorfer@gmx.de>
2020-02-29 19:38:27 +0100

    Some sample queries

---
+++ src/main/scala/query/QueryJob.scala
@@ -0,0 +1,43 @@
+package query
+
+import config.Settings
+import utils.SparkUtils.{getSQLContext, getSparkContext}
+
+object QueryJob {
+
+    val wlc: Settings.WebLogGen.type = Settings.WebLogGen
+
+    def main(args: Array[String]): Unit = {
+
+        val sc = getSparkContext
+        val sqlContext = getSQLContext(sc)
+
+        sqlContext
+            .read
+            .format("org.apache.spark.sql.cassandra")
+            .options(Map("table" -> wlc.defaultMasterLogDataTableName,
+                "keyspace" -> wlc.defaultKeySpace))
+            .load
+            .filter("loglevel == 'error'")
+            .show(10)
+
+        print("Number of entries in master database: ")
+        println(sqlContext
+            .read
+            .format("org.apache.spark.sql.cassandra")
+            .options(Map("table" -> wlc.defaultMasterLogDataTableName,
+                "keyspace" -> wlc.defaultKeySpace))
+            .load
+            .count)
+
+        print("Number of visitors with an old HTTP version: ")
+        println(sqlContext
+            .read
+            .format("org.apache.spark.sql.cassandra")
+            .options(Map("table" -> wlc.defaultMasterLogDataTableName,
+                "keyspace" -> wlc.defaultKeySpace))
+            .load
+            .filter("httpversion = 'HTTP/1.0'")
+            .count)
+    }
+}
