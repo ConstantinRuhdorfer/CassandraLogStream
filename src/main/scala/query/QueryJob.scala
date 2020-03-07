de378920d1638c2f00331447b22e72797feea409
Constantin Ruhdorfer <constantin.ruhdorfer@gmx.de>
2020-03-07 18:35:39 +0100

    Queries for presentation.

--- src/main/scala/query/QueryJob.scala
+++ src/main/scala/query/QueryJob.scala
@@ -1,11 +1,15 @@
 package query
 
-import com.datastax.driver.core.Session
+import com.datastax.driver.core.{Row, Session}
 import config.Settings
+import domainTypes.LogLevel
 import org.apache.spark.SparkContext
 import org.apache.spark.sql.SQLContext
 import utils.CassandraUtils.getOrCreateCassandraConnector
 import utils.SparkUtils.{getSQLContext, getSparkContext}
+
+import scala.collection.JavaConverters._
+import scala.collection.mutable
 
 object QueryJob {
 
@@ -34,15 +38,93 @@ object QueryJob {
         else {
             println("Skip.")
         }
-
     }
 
     /**
-     * Executes a list of queries against cassandra.
+     * Executes a list of queries against
+     * cassandra for presentation purposes.
      */
     def executeStats(): Unit = {
 
-        session.execute("use logstreamcassandra;")
+        session.execute("USE logstreamcassandra;")
+
+        val q1: mutable.Seq[Row] = session
+            .execute(
+                s"""
+                   |SELECT * FROM logs_by_timestamp
+                   |LIMIT 10;
+                   |""".stripMargin)
+            .all
+            .asScala
+
+        val timestamp: Long = q1.head.getLong(0)
+        val q2: mutable.Seq[Row] = session
+            .execute(
+                s"""
+                   |SELECT * FROM logs_by_timestamp
+                   |WHERE timestamp = $timestamp
+                   |LIMIT 20;
+                   |""".stripMargin)
+            .all
+            .asScala
+
+        val q3: mutable.Seq[Row] = session
+            .execute(
+                s"""
+                   |SELECT * FROM logs_by_loglevel
+                   |WHERE loglevel = '${LogLevel.ERROR}'
+                   |LIMIT 20;
+                   |""".stripMargin)
+            .all
+            .asScala
+
+        val someVisitorWhoCausedAnError = q3.head.getString(3)
+        val q4: mutable.Seq[Row] = session
+            .execute(
+                s"""
+                   |SELECT * FROM ips_by_visitorid
+                   |WHERE visitorid = '$someVisitorWhoCausedAnError';
+                   |""".stripMargin)
+            .all
+            .asScala
+
+
+        val q5: mutable.Seq[Row] = session
+            .execute(
+                s"""
+                   |SELECT * FROM viewed_pages_by_visitorid
+                   |WHERE visitorid = '$someVisitorWhoCausedAnError';
+                   |""".stripMargin)
+            .all
+            .asScala
+
+        val viewdPageBySomeVisitorWhoCausedAnError = q5.head.getString(2)
+        val q6: mutable.Seq[Row] = session
+            .execute(
+                s"""
+                   |SELECT * FROM page_details_by_pageid
+                   |WHERE pageid = '$viewdPageBySomeVisitorWhoCausedAnError';
+                   |""".stripMargin)
+            .all
+            .asScala
+
+        val q7: mutable.Seq[Row] = session
+            .execute(
+                s"""
+                   |SELECT visitorid FROM visitors_by_pageid
+                   |WHERE pageid = '$viewdPageBySomeVisitorWhoCausedAnError';
+                   |""".stripMargin)
+            .all
+            .asScala
+
+        // TODO: Correct
+        println(q1)
+        println(q2)
+        println(q3)
+        println(q4)
+        println(q5)
+        println(q6)
+        println(q7)
 
         session.close()
     }
